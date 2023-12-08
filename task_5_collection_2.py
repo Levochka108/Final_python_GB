c12186485b6050b9c683d2332c3d4d4320f72f30
Dmitrii Klochkov <d.klochkov9421@gmail.com>
2023-12-08 14:23:44 +0300

    Решение задачи 5

--- task_5_collection_2.py
+++ task_5_collection_2.py
@@ -1,20 +1,37 @@
-"""
-Задание №5
-Погружение в Python | Коллекции
-✔ Создайте вручную список с повторяющимися целыми числами.
-✔ Сформируйте список с порядковыми номерами
-нечётных элементов исходного списка.
-✔ Нумерация начинается с единицы.
-"""
+import logging
 
-# традиционный итератор с функцией append
-lst = [2, 2, 2, 3, 3, 5]
-lst_numbers = []
-for i in range(len(lst)):
-    if lst[i] % 2 == 1:
-        lst_numbers.append(i + 1)
-print(lst_numbers)
+FILENAME = "logs/logfile_task_5.log"
+FILEMODE = 'w'
+FORMAT = '%(levelname)s - %(asctime)s - %(message)s'
+ENCODING = 'utf-8'
 
-# list comprehension
-lst_numbers = [i + 1 for i in range(len(lst)) if lst[i] % 2 == 1]
-print(lst_numbers)
+# Настройка логгера
+logging.basicConfig(filename=FILENAME,
+                    filemode=FILEMODE,
+                    format=FORMAT,
+                    encoding=ENCODING,
+                    level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+
+def get_odd_indices(lst):
+    return [i + 1 for i in range(len(lst)) if lst[i] % 2 == 1]
+
+
+if __name__ == '__main__':
+    """
+    Задание №5
+    ✔ Создайте вручную список с повторяющимися целыми числами.
+    ✔ Сформируйте список с порядковыми номерами нечётных элементов исходного списка.
+    ✔ Нумерация начинается с единицы.
+    """
+
+    try:
+        lst = [2, 2, 2, 3, 3, 5]
+        lst_numbers = get_odd_indices(lst)
+        logger.info(f'Исходный список: {lst}')
+        logger.info(
+            f'Список порядковых номеров нечетных элементов: {lst_numbers}')
+    except Exception as e:
+        logger.error(f'Произошла ошибка - {e}')
